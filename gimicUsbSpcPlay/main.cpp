a9038f45c512c2166c309c8f92126d8e11e13aeb
osoumen <osoumen@rice-2.local>
2014-06-14 20:30:49 +0900

    Initial Commit

---
+++ gimicUsbSpcPlay/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gimicUsbSpcPlay
+//
+//  Created by Takahiko Fujita on 2014/06/14.
+//  Copyright (c) 2014年 osoumen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
